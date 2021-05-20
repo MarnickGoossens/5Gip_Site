7c0f8e4f7083fb48dd45ba5f6a07a9d2f7473bbc
MarnickGoossens <77274498+MarnickGoossens@users.noreply.github.com>
2021-05-20 15:32:33 +0200

    add path to django + vscode settgins folder added to gitignore

--- main.py
+++ main.py
@@ -9,31 +9,43 @@ settings.configure(
     ROOT_URLCONF=__name__,
 )
 
+
 def home(request):
     home_pagina = open("index.html", "rt")
     home = home_pagina.read()
     return HttpResponse(home)
+
 
 def discription(request):
     discription_pagina = open("discription.html", "rt")
     discription = discription_pagina.read()
     return HttpResponse(discription)
 
+
 def program(request):
     program_pagina = open("program.html", "rt")
     program = program_pagina.read()
     return HttpResponse(program)
+
 
 def calculation(request):
     calculation_pagina = open("calculations.html", "rt")
     calculation = calculation_pagina.read()
     return HttpResponse(calculation)
 
+
+def about(request):
+    about_pagina = open("about.html", "rt")
+    about = about_pagina.read()
+    return HttpResponse(about)
+
+
 urlpatterns = [
     path("", home),
     path("discription", discription),
     path("program", program),
     path("calculation", calculation),
+    path("about", about),
 ]
 
 execute_from_command_line()
